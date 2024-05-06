7501f24aa2a5a4cfcf820da9167be25cab591911
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2024-05-06 19:09:02 +0530

    add const file

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:8000/v1";
-export const API_BASE_URL_FOR_MASTER = "http://localhost:8000/v1/masters";
+export const API_BASE_URL = "https://rec-api-59g8.onrender.com/v1";
+export const API_BASE_URL_FOR_MASTER = "https://rec-api-59g8.onrender.com/v1/masters";
 export const PAGE_LIMIT = 10;
 
 export const apiActions = {

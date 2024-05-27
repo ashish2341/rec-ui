8d89bd8b65fe087c26ea8d52a0ef64c4ea897dd3
Rohan <rohanbehera6274@gmail.com>
2024-05-27 11:39:44 +0530

    add property Ui Changed

--- app/(admin)/property/addProperty/page.js
+++ app/(admin)/property/addProperty/page.js
@@ -10,14 +10,14 @@ import Styles from "./addProperty.module.css";
 import { AddProperty } from "@/api-functions/property/addProperty";
 import { ToastContainer, toast } from "react-toastify";
 import { useRouter } from "next/navigation";
-
+import Stepper from "@/components/common/stepper/stepper";
 export default function AddProject() {
   const [pageValue, setPageValue] = useState(1);
   const [activePage, setActivePage] = useState();
   const [selectedAmenities, setSelectedAmenities] = useState([]);
   const [selectedFeatures, setSelectedFeatures] = useState([]);
   const [valueForBack, setValueForBack] = useState(0);
-
+  const [propertyBackvalue, setPropertyBackvalue] = useState(0);
   const router = useRouter();
   const handleAmenitiesChange = (amenities) => {
     console.log("amenities", amenities);
@@ -38,6 +38,9 @@ export default function AddProject() {
   const handelBackPage = () => {
     setPageValue(pageValue - 1);
     setValueForBack(0);
+    if (pageValue == 3 || pageValue ==4) {
+      setPropertyBackvalue(0);
+    }
   };
 
   const handelBackValue = (value) => {
@@ -48,31 +51,31 @@ export default function AddProject() {
     const propertyData = JSON.parse(sessionStorage.getItem("propertyData"));
     console.log("all Property Data", propertyData);
     console.log("propertyData.Aminities", propertyData?.Aminities);
-    if (propertyData){
-      const loanDetails= {
-        ByBank:propertyData?.LoanDetails?.ByBank.map((item)=>{
-          return item._id
+    if (propertyData) {
+      const loanDetails = {
+        ByBank: propertyData?.LoanDetails?.ByBank.map((item) => {
+          return item._id;
         }),
-        LoanSince:propertyData?.LoanDetails?.LoanSince ,
-        LoanTill:propertyData?.LoanDetails?. LoanTill
-      }
+        LoanSince: propertyData?.LoanDetails?.LoanSince,
+        LoanTill: propertyData?.LoanDetails?.LoanTill,
+      };
       const finalizePropertyData = {
         Titile: propertyData?.Titile,
         Description: propertyData?.Description,
         Highlight: propertyData?.Highlight,
-        Facing: [propertyData?.Facing?.value],
+        Facing: [propertyData?.Facing?._id],
         IsEnabled: propertyData?.IsEnabled,
         IsExclusive: propertyData?.IsExclusive,
         IsFeatured: propertyData?.IsFeatured,
         IsNew: propertyData?.IsNew,
-        ProeprtyFor: propertyData?.ProeprtyFor?.value,
-        PropertyType: propertyData?.PropertyTypeWithSubtype?.value,
+        ProeprtyFor: propertyData?.ProeprtyFor,
+        PropertyType: propertyData?.PropertyTypeWithSubtype?._id,
         Bedrooms: propertyData?.Bedrooms,
         Bathrooms: propertyData?.Bathrooms,
-        Fencing: propertyData?.Fencing?.value,
-        Flooring: propertyData?.Flooring?.value,
-        Furnished: propertyData?.Furnished?.value,
-        BuiltAreaType: propertyData?.BuiltAreaType?.value,
+        Fencing: propertyData?.Fencing?._id,
+        Flooring: propertyData?.Flooring?._id,
+        Furnished: propertyData?.Furnished?._id,
+        BuiltAreaType: propertyData?.BuiltAreaType?._id,
         LandArea: propertyData?.LandArea,
         CoveredArea: propertyData?.CoveredArea,
         CarpetArea: propertyData?.CarpetArea,
@@ -80,7 +83,7 @@ export default function AddProject() {
         PerUnitPrice: propertyData?.PerUnitPrice,
         IsDisplayPrice: propertyData?.IsDisplayPrice,
         IsNegotiable: propertyData?.IsNegotiable,
-        PosessionStatus: propertyData?.PosessionStatus?.value,
+        PosessionStatus: propertyData?.PosessionStatus?._id,
         PosessionDate: propertyData?.PosessionDate,
         FloorNumber: propertyData?.FloorNumber,
         TotalFloors: propertyData?.TotalFloors,
@@ -89,15 +92,15 @@ export default function AddProject() {
         FloorsAllowed: propertyData?.FloorsAllowed,
         IsInterstedInJoinedVenture: propertyData?.IsInterstedInJoinedVenture,
         Balconies: propertyData?.Balconies,
-        Soil: propertyData?.Soil?.value,
+        Soil: propertyData?.Soil?._id,
         IsLoanable: propertyData?.IsLoanable,
         IsAlreadyLoaned: propertyData?.IsAlreadyLoaned,
         LoanDetails: loanDetails,
-        OwnershipType: propertyData?.OwnershipType?.value,
-        PropertyStatus: propertyData?.PropertyStatus?.value,
+        OwnershipType: propertyData?.OwnershipType?._id,
+        PropertyStatus: propertyData?.PropertyStatus?._id,
         IsSold: propertyData?.IsSold,
-        Preferences: propertyData?.Preferences.map((item)=>{
-          return item._id
+        Preferences: propertyData?.Preferences.map((item) => {
+          return item._id;
         }),
         DiscountPercentage: propertyData?.DiscountPercentage,
         DiscountForYears: propertyData?.DiscountForYears,
@@ -109,22 +112,22 @@ export default function AddProject() {
         State: propertyData?.State,
         Country: propertyData?.Country,
         Address: propertyData?.Address,
-        Area: propertyData?.Area.value,
-        PinCode:propertyData.PinCode,
-        Landmark:propertyData.Landmark,
+        Area: propertyData?.Area?._id,
+        PinCode: propertyData.PinCode,
+        Landmark: propertyData.Landmark,
         Location: propertyData?.Location,
         Images: propertyData?.Images.map((URL) => ({ URL })),
         Videos: propertyData?.Videos.map((URL) => ({ URL })),
         Documents: propertyData?.Documents.map((URL) => ({ URL })),
-        AreaUnits:propertyData?.AreaUnits?.value,
-        ReraNumber:propertyData?.ReraNumber,
-        BhkType:propertyData?.BhkType?.value,
-        FloorAndCounter:propertyData?.FloorAndCounter,
-        Fitting:propertyData?.Fitting,
-        WallAndCeiling:propertyData?.WallAndCeiling,
-        Faq:propertyData?.Faq,
-        Brochure:propertyData?.Brochure,
-        Builder:propertyData?.Builder?.value
+        AreaUnits: propertyData?.AreaUnits?._id,
+        ReraNumber: propertyData?.ReraNumber,
+        BhkType: propertyData?.BhkType?._id,
+        FloorAndCounter: propertyData?.FloorAndCounter,
+        Fitting: propertyData?.Fitting,
+        WallAndCeiling: propertyData?.WallAndCeiling,
+        Faq: propertyData?.Faq,
+        Brochure: propertyData?.Brochure,
+        Builder: propertyData?.Builder?._id,
       };
       console.log("finalizePropertyData", finalizePropertyData);
       let res = await AddProperty(finalizePropertyData);
@@ -139,197 +142,108 @@ export default function AddProject() {
         toast.error(res.errMessage);
         return false;
       }
-       }
-   
-   
+    }
   };
+
+  const stepperArray = [
+    "Basic details",
+    "Property details",
+    "Feature/Amenity",
+    "Location details",
+    "Faq details",
+    "Property Images",
+  ];
+  console.log("propertyBackvalue", propertyBackvalue);
+  console.log("pageValue", pageValue);
   return (
     <section>
-      <h1 className="text-2xl text-black-600 underline mb-3 font-bold">
-        Add Your Property Details
-      </h1>
+      <div className="flex">
+        <h1 className="text-2xl text-black-600 underline mb-3 font-bold">
+          Add Your Property Details
+        </h1>
+        {pageValue > 1 ? (
+          pageValue == 1 ? null : (pageValue == 2 && propertyBackvalue == 0) ||
+            (pageValue == 3 && propertyBackvalue == 0) ? (
+            <button
+              onClick={handelBackPage}
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-20"
+            >
+              Back
+            </button>
+          ) : pageValue == 1 || pageValue == 2 || pageValue == 3 ? (
+           null
+          ) : ( <button
+            onClick={handelBackPage}
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-20"
+          >
+            Back
+          </button>)
+        ) : null}
+      </div>
 
-      <ol className="flex items-center w-full mb-4 sm:mb-5">
-        <li className="w-full">
-          <div
-            className={`${
-              pageValue == 2 || pageValue > 2 ? Styles.activeMain : ""
-            } flex w-full items-center text-black-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-500 after:border-4 after:inline-block dark:after:border-blue-800`}
-          >
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 1
-                  ? Styles.actiivetab
-                  : pageValue > 1
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              1
-            </div>
+      <div className="container mx-auto p-4">
+        <div className={`${Styles.propertyContainer}`}>
+          <div className={`${Styles.column1}`}>
+            <Stepper steppers={stepperArray} pageNumber={pageValue} />
           </div>
-          <p>Basic details</p>
-        </li>
-        <li className="w-full">
-          <div
-            className={`${
-              pageValue == 3 || pageValue > 3 ? Styles.activeMain : ""
-            } flex w-full items-center text-black-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800`}
-          >
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 2
-                  ? Styles.actiivetab
-                  : pageValue > 2
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              2
-            </div>
+
+          <div className={`${Styles.column2}`}>
+            {pageValue === 1 && (
+              <BasicDetailsForm
+                valueForNext={handelNextBtnValue}
+                valueForNextPage={pageValue}
+              />
+            )}
+            {pageValue === 2 && (
+              <PropertyDetailsForm
+                setPropertyBackvalue={setPropertyBackvalue}
+                valueForNext={handelNextBtnValue}
+                valueForNextPage={pageValue}
+              />
+            )}
+            {pageValue === 3 && (
+              <FeaturesDetailsForm
+                setPropertyBackvalue={setPropertyBackvalue}
+                onAmenitiesChange={handleAmenitiesChange}
+                onFeaturesChange={handleFeaturesChange}
+                valueForNext={handelNextBtnValue}
+                valueForNextPage={pageValue}
+              />
+            )}
+            {pageValue === 4 && (
+              <LocationDetailsForm
+                valueForNext={handelNextBtnValue}
+                valueForNextPage={pageValue}
+              />
+            )}
+            {pageValue === 5 && (
+              <PropertyFaqForm
+                valueForNext={handelNextBtnValue}
+                valueForNextPage={pageValue}
+              />
+            )}
+            {pageValue === 6 && (
+              <PropertyImagesForm
+                valueForNextPage={pageValue}
+                valueForBack={handelBackValue}
+                mainBackPageValue={valueForBack}
+              />
+            )}
+
+            {valueForBack === 1 && (
+              <button
+                onClick={submitPropertyData}
+                type="button"
+                className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-5"
+              >
+                Finish
+              </button>
+            )}
           </div>
-          <p>Property details</p>
-        </li>
-        <li className="w-full">
-          <div
-            className={`${
-              pageValue == 4 || pageValue > 4 ? Styles.activeMain : ""
-            } flex w-full items-center text-black-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800`}
-          >
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 3
-                  ? Styles.actiivetab
-                  : pageValue > 3
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              3
-            </div>
-          </div>
-          <p>Feature/Amenity</p>
-        </li>
-        <li className="w-full">
-          <div
-            className={`${
-              pageValue == 5 || pageValue > 5 ? Styles.activeMain : ""
-            } flex w-full items-center text-black-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800`}
-          >
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 4
-                  ? Styles.actiivetab
-                  : pageValue > 4
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              4
-            </div>
-          </div>
-          <p>Location details</p>
-        </li>
-        <li className="w-full">
-          <div
-            className={`${
-              pageValue == 6 || pageValue > 6 ? Styles.activeMain : ""
-            } flex w-full items-center text-black-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800`}
-          >
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 5
-                  ? Styles.actiivetab
-                  : pageValue > 5
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              5
-            </div>
-          </div>
-          <p>Faq details</p>
-        </li>
-        <li className="w-full">
-          <div className={` flex items-center text-black-600 w-full`}>
-            <div
-              className={`flex items-center justify-center w-10 h-10 ${
-                pageValue == 6
-                  ? Styles.actiivetab
-                  : pageValue > 6
-                  ? "bg-blue-500"
-                  : "bg-blue-100"
-              } rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0`}
-            >
-              6
-            </div>
-          </div>
-          <p>Property Images</p>
-        </li>
-        
-        {/* <li className="flex w-full items-center text-black-600 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-700">
-        <div className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-            2
         </div>
-    </li>
-    <li className="flex items-center text-black-600 w-full">
-        <div className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-            3
-        </div>
-    </li> */}
-      </ol>
-      {pageValue > 1 ? (
-        <button
-          onClick={handelBackPage}
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5 mb-5"
-        >
-          Back
-        </button>
-      ) : null}
-
-      {pageValue == 1 ? (
-        <BasicDetailsForm
-          valueForNext={handelNextBtnValue} //here it's value comes from child component
-          valueForNextPage={pageValue} //here its value pass from parent to child component
-        />
-      ) : pageValue == 2 ? (
-        <PropertyDetailsForm
-          valueForNext={handelNextBtnValue} //here it's value comes from child component
-          valueForNextPage={pageValue} //here its value pass from parent to child component
-        />
-      ) : pageValue == 3 ? (
-        <FeaturesDetailsForm
-          onAmenitiesChange={handleAmenitiesChange}
-          onFeaturesChange={handleFeaturesChange}
-          valueForNext={handelNextBtnValue} //here it's value comes from child component
-          valueForNextPage={pageValue} //here its value pass from parent to child component
-        />
-      ) : pageValue == 4 ? (
-        <LocationDetailsForm
-          valueForNext={handelNextBtnValue} //here it's value comes from child component
-          valueForNextPage={pageValue} //here its value pass from parent to child component
-        />
-      ) :  pageValue == 5 ?(
-        <PropertyFaqForm
-          valueForNext={handelNextBtnValue} //here it's value comes from child component
-          valueForNextPage={pageValue} //here its value pass from parent to child component
-        />
-      ):<PropertyImagesForm
-      valueForNextPage={pageValue} //here its value pass from parent to child component
-      valueForBack={handelBackValue} //here it's value comes from child component
-      mainBackPageValue={valueForBack} //here its value pass from parent to child component
-    />}
-
-      {valueForBack == 1 ? (
-        <button
-          onClick={submitPropertyData}
-          type="button"
-          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-5"
-        >
-          Finish
-        </button>
-      ) : null}
+      </div>
     </section>
   );
 }
---
+++ components/admin/propertyAddForms/featureDetails/parts/partTwo.js
@@ -0,0 +1,112 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { API_BASE_URL_FOR_MASTER } from "@/utils/constants";
+import useFetch from "@/customHooks/useFetch";
+import Cookies from "js-cookie";
+import Styles from "../featurepage.module.css";
+import ContinueButton from "@/components/common/propertyContinueButton/continueButton";
+
+export default function PartTwo({ setPropertyPageValue }) {
+
+  // fetching Data for Features
+  const { data: featuresData } = useFetch(
+    `${API_BASE_URL_FOR_MASTER}/features`
+  );
+  console.log("featuresData", featuresData);
+
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
+  useEffect(() => {
+    // Retrieve data from localStorage
+    const sessionStoragePropertyData = JSON.parse(
+      sessionStorage.getItem("propertyData")
+    );
+    console.log(
+      "localStorageData from localstorage",
+      sessionStoragePropertyData
+    );
+    // Update state values if data exists in localStorage
+    if (sessionStoragePropertyData) {
+   
+      setSelectedFeatures(sessionStoragePropertyData?.Features || []);      
+       
+    }
+  }, []);
+
+  const handlefeatureChange = (itemId) => {
+    setSelectedFeatures((prev) => {
+      const isSelected = prev.some(
+        (selectedItemId) => selectedItemId === itemId
+      );
+      if (isSelected) {
+        return prev.filter((selectedItemId) => selectedItemId !== itemId);
+      } else {
+        return [...prev, itemId];
+      }
+    });
+  };
+  const SubmitForm = () => {
+    if (selectedFeatures.length == 0) {
+      toast.error("Please select a Amenity.");
+      return false;
+    }
+    const featureData = {
+      
+      Features: selectedFeatures,
+
+    };
+    console.log("featureData", featureData);
+    const localStorageData = JSON.parse(sessionStorage.getItem("propertyData"));
+    const newProjectData = { ...localStorageData, ...featureData };
+    sessionStorage.setItem("propertyData", JSON.stringify(newProjectData));
+      setPropertyPageValue((prev) => prev + 1);
+    
+   
+  };
+  return (
+    <>
+     <div className={`flex justify-end ${Styles.continueBtn}`}>
+        <ContinueButton modalSubmit={SubmitForm} />
+      </div>
+
+      <div className={`${Styles.featurebox} container mx-auto px-4 py-8`}>
+        {/* Amenity Box */}
+        <div className="bg-white rounded-lg shadow-md p-6 mb-6">
+          <h2 className="text-xl font-semibold mb-4">Feature</h2>
+          {featuresData && selectedFeatures ? (
+            <div>
+              {featuresData ? (
+                <div
+                  className={`flex flex-wrap space-x-2 mt-4 ${
+                    featuresData?.data.length > 20 ? "scrollable" : ""
+                  }`}
+                >
+                  {featuresData?.data.map((item) => (
+                    <button
+                      key={item._id}
+                      onClick={() => handlefeatureChange(item._id)}
+                      className={`rounded text-white px-4 py-2 ${
+                        Styles.optionButton
+                      } ${
+                        selectedFeatures.some(
+                          (selectedItemId) => selectedItemId === item._id
+                        )
+                          ? "bg-[#2a4acb] border-2 border-[#2a4acb]"
+                          : "bg-[#6592d3] border-2 border-[#6592d3]"
+                      }`}
+                    >
+                      {item.Feature}
+                    </button>
+                  ))}
+                </div>
+              ) : (
+                null
+              )}
+            </div>
+          ) : 
+          null}
+        </div>
+      </div>
+    </>
+  );
+}
